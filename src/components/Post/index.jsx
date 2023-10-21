7f1b2d76e8c1b1d93280e422cadc5b662ed7a679
Vika <viktoria.wout@gmail.com>
2023-10-21 15:04:43 +0200

    id params added to implement one post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -12,7 +12,7 @@ import { UserInfo } from '../UserInfo';
 import { PostSkeleton } from './Skeleton';
 
 export const Post = ({
-  _id,
+  id,
   title,
   createdAt,
   imageUrl,
@@ -35,7 +35,7 @@ export const Post = ({
     <div className={clsx(s.root, { [s.rootFull]: isFullPost })}>
       {isEditable && (
         <div className={s.editButtons}>
-          <Link to={`/posts/${_id}/edit`}>
+          <Link to={`/posts/${id}/edit`}>
             <IconButton color="primary">
               <EditIcon />
             </IconButton>
@@ -56,7 +56,7 @@ export const Post = ({
         <UserInfo {...user} additionalText={createdAt} />
         <div className={s.indention}>
           <h2 className={clsx(s.title, { [s.titleFull]: isFullPost })}>
-            {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
+            {isFullPost ? title : <Link to={`/posts/${id}`}>{title}</Link>}
           </h2>
           <ul className={s.tags}>
             {tags.map((name) => (
